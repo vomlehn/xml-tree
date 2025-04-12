9462594814eeb21f6e602dca260bdce60ab83f63
David VomLehn <dvomlehn@gmail.com>
2025-04-12 14:57:56 -0500

    Re-implement Walkstatus and Elementstatus as derivatives of trait Try.

--- src/walkable.rs
+++ src/walkable.rs
@@ -15,13 +15,14 @@ use crate::xml_document::{Element, XmlDocument};
  * RET2     Trait for Try value from end() and walk_n()
  */
 // -----------------------------------------
-use std::error::Error;
-use std::fmt;
-use std::ops::Try;
+//use std::error::Error;
+////use std::fmt;
+use std::ops::{ControlFlow, Try};
 
 mod tests {
     use std::collections::BTreeMap;
-    use std::marker::PhantomData;
+    use std::ops::{FromResidual};
+//    use std::marker::PhantomData;
     use crate::xml_document::{Element, ElementInfo, XmlDocument};
     use crate::xml_document_factory::DocumentInfo;
     use xml::attribute::OwnedAttribute;
@@ -43,8 +44,11 @@ mod tests {
         };
 
         let e4: Element = Element {
-            name:           OwnedName { local_name: "n4".to_string(), namespace: None,
-                                prefix: None},
+            name:           OwnedName {
+                local_name: "n4".to_string(),
+                namespace: None,
+                prefix: None,
+            },
             depth:          0,
             element_info:   ei.clone(),
             subelements:    Vec::<Element>::new(),
@@ -121,10 +125,12 @@ println!("calling test1");
         let res_a = a.walk(&elemdata_a);
         println!("Result:\n{:?}", res_a);
 
-        assert_eq!(res_a.unwrap().data, "n1\n".to_owned() +
+/*
+        assert_eq!(res_a.data, "n1\n".to_owned() +
             INDENT + "n2\n" +
             INDENT + "n3\n" +
             INDENT + INDENT + "n4");
+*/
     }
 
     struct A<'a> {
@@ -140,73 +146,54 @@ println!("calling test1");
         }
     }
 
-    /*
-        impl<'a, AC: Accumulator, WD: WalkData> Walkable<AC, WD> for A<'a, AC, WD> {
-    //        type AC = dyn Accumulator<WD = WalkdataA>;
-    //        type AS = Result<Self::AC, Box<dyn Error>>;
-            type ED = ElementdataA;
-            type ES = Result<Self::ED, Box<dyn Error>>;
-    //        type WD = u8;
-            type WS = Result<WD, Box<dyn Error>>;
-
-            fn xml_document(&self) -> &XmlDocument {
-                self.xml_document
-            }
-        }
-    */
-
-    /*
-        impl WalkdataA {
-        }
-
-        impl WalkData for WalkdataA {
-        }
-    */
-
-    /*
-        impl ElementData for ElementdataA {
-            type WD = u8;
-            type WS = Result<Self::WD, Box<dyn Error>>;
-
-            fn summary(&self) -> Self::WS {
-                Ok(37)
-            }
-        }
-    */
-
-    //     struct B<'a> {
-    //         xml_document:   &'a XmlDocument,
-    //     }
-    // 
-    //     impl<'a> Walkable for B<'a> {
-    //         type ED = Bd;
-    //         type ES = Result<Self::ED, Box<dyn Error>>;
-    // //        type WD = ();
-    //         type WS = Result<Self::WD, Box<dyn Error>>;
-    // 
-    //         fn xml_document(&self) -> &XmlDocument {
-    //             self.xml_document
-    //         }
-    //     }
-    // 
-    //     struct Bd {
-    //     }
-    // 
-    //     impl ElementData for Bd {
-    //         type WD = ();
-    //         type WS = fmt::Result;
-    // 
-    //         fn summary(&self) -> Self::WS {
-    //             Ok(())
-    //         }
-    //     }
-
     #[derive(Debug)]
     pub struct WalkdataA {
         data:   String,
     }
 
-    pub type WalkstatusA<T, E> = Result<T, Box<E>>;
+    #[derive(Debug)]
+    pub enum WalkResultA<T, E> {
+        Err(E),
+        Ok(T),
+    }
+
+    impl<'a, T, E> WalkResultA<T, E> {
+        #[allow(dead_code)]
+        fn is_ok(&self) -> bool {
+            matches!(self, WalkResultA::Ok(_))
+        }
+        
+        #[allow(dead_code)]
+        fn is_err(&self) -> bool {
+            matches!(self, WalkResultA::Err(_))
+        }
+    }
+
+    impl<'a, T, E> FromResidual<Result<std::convert::Infallible, E>> for
+        WalkResultA<T, E> {
+        fn from_residual(residual: Result<std::convert::Infallible, E>) -> Self {
+            match residual {
+                Result::Err(e) => WalkResultA::Err(e),
+                Result::Ok(_) => unreachable!(),
+            }
+        }
+    }
+
+    impl<'a, T, E> Try for WalkResultA<T, E> {
+        type Output = T;
+        type Residual = Result<std::convert::Infallible, E>;
+
+        fn from_output(output: Self::Output) -> Self {
+            WalkResultA::Ok(output)
+        }
+
+        fn branch(self) -> ControlFlow<Self::Residual, Self::Output> {
+            match self {
+               WalkResultA::Ok(val) => ControlFlow::Continue(val),
+               WalkResultA::Err(err) => ControlFlow::Break(Err(err)),
+            }
+        }
+    }
 
     /*
      * This has to be a trait so the functions can be defined by users
@@ -214,49 +201,88 @@ println!("calling test1");
     pub struct ElementdataA {
         depth:  usize,
     //    f:      &'a mut fmt::Formatter<'a>,
-    //    type WD;
-    //    type WS: Try;
 
-    /*
-        fn end(&mut self, element: &Element,
-            subelements: Vec<Box<dyn W>>) ->
-            Result<Box<dyn W>, Box<dyn Error>>;
-    */
     }
 
     impl<'a> ElementdataA {
         pub fn start(&self, element: &Element) ->
-            ElementstatusA<ElementdataA, dyn Error> {
+            ElementResultA<ElementdataA, String> {
             println!("{}{}", INDENT.repeat(self.depth), element.name);
-            Ok(ElementdataA {
+            ElementResultA::<_, _>::Ok(ElementdataA {
                 depth:  self.depth + 1,
     //            f:      element.f,
             })
         }
     }
 
-    pub type ElementstatusA<T, E> = Result<T, Box<E>>;
+    #[derive(Debug)]
+    pub enum ElementResultA<T, E> {
+        Err(E),
+        Ok(T),
+    }
+
+    impl<'a, T, E> ElementResultA<T, E> {
+        #[allow(dead_code)]
+        fn is_ok(&self) -> bool {
+            matches!(self, ElementResultA::Ok(_))
+        }
+        
+        #[allow(dead_code)]
+        fn is_err(&self) -> bool {
+            matches!(self, ElementResultA::Err(_))
+        }
+    }
+
+    impl<'a, T, E> FromResidual<Result<std::convert::Infallible, E>> for
+        ElementResultA<T, E> {
+        fn from_residual(residual: Result<std::convert::Infallible, E>) -> Self {
+            match residual {
+                Result::Err(e) => ElementResultA::Err(e),
+                Result::Ok(_) => unreachable!(),
+            }
+        }
+    }
+
+    impl<'a, T, E> Try for ElementResultA<T, E> {
+        type Output = T;
+        type Residual = Result<std::convert::Infallible, E>;
+
+        fn from_output(output: Self::Output) -> Self {
+            ElementResultA::Ok(output)
+        }
+
+        fn branch(self) -> ControlFlow<Self::Residual, Self::Output> {
+            match self {
+               ElementResultA::Ok(val) => ControlFlow::Continue(val),
+               ElementResultA::Err(err) => ControlFlow::Break(Err(err)),
+            }
+        }
+    }
 
     #[derive(Debug)]
     pub struct AccumulatorA {
-        depth:  usize,
         result: String,
     }
 
-    impl AccumulatorA {
+    impl<'a> AccumulatorA {
         pub fn new(e: &Element, ed: &ElementdataA) -> Self {
             let result = INDENT.repeat(ed.depth) + &e.name.local_name.clone();
             AccumulatorA {
-                depth:  ed.depth,
                 result: result,
             }
         }
-        pub fn add(&mut self, ws: WalkdataA) -> WalkstatusA<(), dyn Error> {
+
+        // FIXME: what should this return?
+        pub fn add(&mut self, ws: WalkdataA) -> WalkResultA<WalkdataA, String> {
             self.result += &("\n".to_owned() + &ws.data);
-            Ok(())
+            WalkResultA::<WalkdataA, String>::Ok(WalkdataA {
+                // FIXME: is there a performance impact here?
+                data: self.result.clone()
+            })
         }
-        pub fn summary(&self) -> WalkstatusA <WalkdataA, dyn Error>{
-            Ok(WalkdataA {
+
+        pub fn summary(&self) -> WalkResultA<WalkdataA, String>{
+            WalkResultA::<_, _>::Ok(WalkdataA {
                 data:   self.result.clone(),
             })
         }
@@ -267,14 +293,14 @@ pub trait Walkable
     {
     fn xml_document(&self) -> &XmlDocument;
         
-    fn walk(&self, d: &tests::ElementdataA) -> tests::WalkstatusA<tests::WalkdataA, dyn Error> {
+    fn walk<'a>(&self, d: &tests::ElementdataA) -> tests::WalkResultA<tests::WalkdataA, String> {
         let document = self.xml_document();
         let e = &document.root;
         self.walk_i(&e, &d)
     }
 
-    fn walk_i(&self, e: &Element, ed: &tests::ElementdataA) ->
-        tests::WalkstatusA<tests::WalkdataA, dyn Error> {
+    fn walk_i<'a>(&self, e: &Element, ed: &tests::ElementdataA) ->
+        tests::WalkResultA<tests::WalkdataA, String> {
         let subd = ed.start(e)?;
         let mut d = tests::AccumulatorA::new(e, ed);
 
@@ -289,76 +315,3 @@ pub trait Walkable
 }
 
 pub trait WalkData {}
-/*
-
-pub trait Walkable {
-    fn xml_document(&self) -> &XmlDocument;
-
-    fn walk<'a, ED: ElementData>(&'a mut self, element_data: &'a mut ED) ->
-        Result<dyn WalkData + 'a, Box<dyn Error + 'a>>
-    {
-        let root = &self.xml_document().root;
-        self.walk_i(root, element_data)
-    }
-
-    fn walk_i<'a, ED: ElementData>(&self, element: &'a Element,
-        element_data: &'a mut ED) ->
-        Result<dyn WalkData + 'a, Box<dyn Error + 'a>>
-    {
-        let mut subelements = Vec::new();
-        let mut d = Vec::new();
-
-        for subelement in &element.subelements {
-            let mut element_subdata = element_data.clone();
-            // Pass the same mutable reference to avoid overlapping borrows
-            let subdata = self.walk_i(subelement, &mut element_subdata)?;
-            let s = subdata;
-            subelements.push(Box::new(s));
-            d.push(element_data.clone());
-        }
-
-        let e = element.clone();
-        element_data.end(&e, subelements)
-    }
-}
-*/
-
-/*
-pub struct PrintWalk<'a> {
-    pub document: &'a XmlDocument,
-//    pub f: &'a mut fmt::Formatter<'a>,
-}
-
-impl<'a> Walkable for PrintWalk<'a> {
-    fn xml_document(&self) -> &XmlDocument {
-        self.document
-    }
-}
-
-pub struct PrintWalkData<'a, 'b> {
-    depth: usize,
-//    f: &'a mut fmt::Formatter<'b>,
-}
-
-impl<'a, 'b> WalkData for () {}
-
-impl<'a, 'b> ElementData for PrintWalkData<'a, 'b> {
-    type StartStatus = Result<Self, Box<dyn Error>>;
-    type EndStatus = Result<(), Box<dyn Error>>;
-
-    fn element_start(&mut self, element: &Element) -> Self::StartStatus {
-        writeln!(self.f, "{}<{}>", "  ".repeat(self.depth), element.name)
-            .map_err(|e| Box::new(e) as Box<dyn Error>);
-
-        Ok(PrintWalkData {
-            depth: self.depth + 1,
-//            f: self.f,
-        })
-    }
-
-    fn end(&mut self, element: &Element, _: Vec<()>) -> Self::EndStatus {
-        writeln!(self.f, "{}<\{}>", "  ".repeat(self.depth), element.name)
-            .map_err(|e| Box::new(e) as Box<dyn Error>)
-    }
-}
-*/
