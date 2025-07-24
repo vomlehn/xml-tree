1df0526a26e16417a71f32a505ac21483538012b
David VomLehn <dvomlehn@gmail.com>
2025-07-23 21:19:32 -0500

    Looks like recursive tree construction works.

--- src/xml_document_factory.rs
+++ src/xml_document_factory.rs
@@ -45,10 +45,6 @@ impl<'a, R: Read + 'a> XmlDocumentFactory<'_, R> {
             xml_schema: xml_schema,
         };
 
-/*
-        let document_info = xml_factory.parse_start_document()?;
-        let before_pieces = self.parse_pieces()?;
-*/
         let xml_document = xml_factory.parse_document::<T>();
         xml_document
     }
@@ -56,18 +52,22 @@ impl<'a, R: Read + 'a> XmlDocumentFactory<'_, R> {
     fn parse_document<T: Read + 'a>(&mut self) -> Result<XmlDocument, XmlDocumentError> {
         let document_info = self.parse_start_document()?;
 
+        // Read the next XML event, which is expected to be the start of an element. We use a
+        // lookahead so that we can be specific about an error if one occurred
         let xml_element = self.parser.lookahead()?;
-println!("parse_document: {:?}", xml_element);
-        let event = xml_element.event.clone();
-        let top_element = if let XmlEvent::StartElement{name, attributes, namespace} = xml_element.event {
-            self.parser.skip();
-            let element_info = ElementInfo::new(xml_element.lineno, attributes, namespace);
-            self.parse_element(name, element_info)?
-        } else {
-            panic!("FIXME: Expected element, got {:?}", xml_element.event);
+
+        let top_element = match xml_element.event {
+            XmlEvent::StartElement{name, attributes, namespace} => {
+                let element_info = ElementInfo::new(xml_element.lineno, attributes, namespace);
+                self.parse_element(name, element_info, 0)?
+            },
+
+            _ => panic!("FIXME: Expected element, got {:?}", xml_element.event),
         };
-            
-        self.end_element(&event)?;
+//println!("parse_document: top_element.subelements().len() {}", top_element.subelements().len());
+
+        self.parse_end_document()?;
+
         Ok(XmlDocument::new(document_info, vec!(top_element)))
     }
 
@@ -76,7 +76,6 @@ println!("parse_document: {:?}", xml_element);
      */
     fn parse_start_document(&mut self) -> Result<DocumentInfo, XmlDocumentError> {
         let xml_element = self.parser.next()?;
-println!("parse_start_document: {:?}", xml_element);
 
         if let XmlEvent::StartDocument{version, encoding, standalone} = xml_element.event {
             Ok(DocumentInfo::new(version, encoding, standalone))
@@ -88,30 +87,80 @@ println!("parse_start_document: {:?}", xml_element);
     /*
      * Parse an element. We have already seen the XmlStartElement as a lookahead.
      */
-    fn parse_element(&mut self, name: OwnedName, element_info: ElementInfo) ->
+    fn parse_element(&mut self, name: OwnedName, element_info: ElementInfo, depth: usize) ->
         Result<Box<dyn Element>, XmlDocumentError> {
+//println!("{}<{}>: start-element", "   ".repeat(depth), name.local_name);
         self.parser.skip();
         let mut element = DirectElement::new(name, element_info, vec!(), vec!(), vec!(), vec!());
-        let mut subelements = Vec::new();
+        let mut open_subelement: Option<Box<dyn Element>> = None;
+//        let mut n: Option<String> = None;
 
+        // Now parse all subelements of this element until we get to the EndElement for this
+        // element.
         loop {
+//println!("{}1-subelements.len() for <{}>: {}", "   ".repeat(depth), element.name(), element.subelements().len());
             let xml_element = self.parser.lookahead()?;
-println!("parse_element: {:?}", xml_element);
+
+/*
+let event = xml_element.event.clone();
+match event {
+    XmlEvent::StartElement{name, ..} => println!("--> {}: start", name.local_name),
+    XmlEvent::EndElement{name} => println!("--> {}: end", name.local_name),
+    _ => {},
+}
+*/
+
             match xml_element.event {
                 XmlEvent::StartElement{name, attributes, namespace} => {
-                    self.parser.skip();
+//println!("{}start-subelement <{}> for <{}>", "   ".repeat(depth), name.local_name, element.name());
+
+                    if open_subelement.is_some() {
+                        panic!("FIXME: element <{}> definition should be closed before defining <{}>",
+                            element.name(), open_subelement.unwrap().name());
+                    }
+
                     let element_info = ElementInfo::new(xml_element.lineno, attributes, namespace);
-                    let subelement = self.parse_element(name, element_info)?;
-                    subelements.push(subelement);
+//                    n = Some(name.local_name.clone());
+                    let subelement = self.parse_element(name, element_info, depth + 1)?;
+                    open_subelement = Some(subelement);
                 },
 
                 XmlEvent::EndElement{name} => {
-                    self.parser.skip();
-                    if element.name() != name.local_name {
-                        panic!("FIXME: name of element <{}> at {} does not match name of closing element <{}> at {}", element.name(), element.lineno(), name.local_name, xml_element.lineno);
+
+                    // If we are not in an element, this end element is for the element we are
+                    // entered this function to parse
+
+                    match open_subelement {
+                        None => {
+//println!("{}<{:?}>: pop", "   ".repeat(depth), element.name());
+                                break;
+                        },
+                        Some(subelement) => {
+                            self.parser.skip();
+                            if name.local_name != subelement.name() {
+                                panic!("FIXME: name of element <{}> at {} does not match name of closing element <{}> at {}", name, xml_element.lineno, subelement.name(), subelement.lineno());
+                            }
+
+                            element.subelements_mut().push(subelement);
+/*
+print!("{}end-subelement {} for {}: [", "   ".repeat(depth), name.local_name, element.name());
+for e in element.subelements() {
+    print!(" {}", e.name());
+}
+println!("]");
+*/
+                            open_subelement = None;
+//n = None;
+                        },
                     }
-                    break;
+//println!("{}2-subelements.len() for <{}>: {}", "   ".repeat(depth), element.name(), element.subelements().len());
                 },
+
+                XmlEvent::EndDocument => {
+                    if open_subelement.is_some() {
+                        panic!("FIXME: element <{}> at {} is not closed", element.name(), element.lineno());
+                    }
+                }
 
                 XmlEvent::Whitespace(_) |
                     XmlEvent::Characters(_) => {
@@ -122,18 +171,39 @@ println!("parse_element: {:?}", xml_element);
             }
         }
 
-        element.subelements_mut().append(&mut subelements);
+/*
+println!("{}3-subelements.len() for <{}>: {}", "   ".repeat(depth), element.name(), element.subelements().len());
+print!("{}<{}>: end-element", "   ".repeat(depth), element.name());
+for e in element.subelements() {
+    print!(" {}", e.name());
+}
+println!();
+*/
+//println!("{}</{:?}>: start_element", "   ".repeat(depth), element.name());
         Ok(Box::new(element))
     }
 
     /*
      * We expect EndDocument, parsed as a lookahead
      */
-    fn end_element(&mut self, event: &XmlEvent) -> Result<(), XmlDocumentError> {
-        if let XmlEvent::EndDocument = event {
-            self.parser.skip();
-            return Ok(())
+    fn parse_end_document(&mut self) -> Result<(), XmlDocumentError> {
+//println!("EndDocument");
+        self.parser.skip();
+
+//println!();
+        loop {
+            let xml_element = self.parser.next()?;
+//println!("end_element: {:?}", xml_element.event);
+            match xml_element.event {
+                XmlEvent::Whitespace(_) |
+                    XmlEvent::Characters(_) => {},
+
+                XmlEvent::EndDocument => break,
+
+                _ => panic!("FIXME: Expected end of document but found {:?}", xml_element.event)
+            }
         }
-        panic!("FIXME: Expected end of document but found {:?}", event)
+
+        Ok(())
     }
 }
